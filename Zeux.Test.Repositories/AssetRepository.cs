8b21cb8660ad7bf66e5e4d2ff5f4169cf4a0e222
r.kupchyn <r.kupchyn@fozzy.ua>
2019-08-08 09:48:03 +0300

    For all apis that return a list of products, have it alphabetically sorted (from a to z).

--- Zeux.Test.Repositories/AssetRepository.cs
+++ Zeux.Test.Repositories/AssetRepository.cs
@@ -8,14 +8,16 @@ namespace Zeux.Test.Repositories
     {
         private readonly FakeContext _context = new FakeContext();
 
+        private IQueryable<Asset> OrderedAssets => _context.Assets.OrderBy(x => x.Name);
+
         public async Task<IQueryable<Asset>> Get()
         {
-            return await Task.Run(() => _context.Assets);
+            return await Task.Run(() => OrderedAssets);
         }
 
         public async Task<IQueryable<Asset>> Get(string type)
         {
-            return await Task.Run(() => _context.Assets.Where(row => row.Type.Name.ToLower() == type.ToLower()));
+            return await Task.Run(() => OrderedAssets.Where(row => row.Type.Name.ToLower() == type.ToLower()));
         }
 
         public async Task<IQueryable<AssetType>> GetTypes()
